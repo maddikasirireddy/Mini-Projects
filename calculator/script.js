0173f8fa7c224bf5c6d1859cda1c7fe209f78ba3
Siri <maddikasirireddy@gmail.com>
2026-03-02 00:53:15 +0530

    calculator added

--- calculator/script.js
+++ calculator/script.js
@@ -1,2 +1,18 @@
-let inputText = document.getElementById("input")
+let display = document.getElementById("display")
 let buttons = document.getElementsByClassName("buttons")
+
+function clicktodisplay(x) {
+    display.value+=x
+}
+
+function tocalculate() {
+    display.value=eval(display.value)
+}
+function toclear() {
+    try {
+        display.value = ""
+    }
+    catch (error) {
+        display.value="Error"
+    }
+}
